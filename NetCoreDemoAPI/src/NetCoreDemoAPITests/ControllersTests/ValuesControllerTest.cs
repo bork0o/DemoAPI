e87d7d3096cb2afa5e4f81538fd6c70e0761fc96
bork0o <bork0o0131@gmail.com>
2017-01-09 17:00:09 +0200

    Added Tests Project

---
+++ NetCoreDemoAPI/src/NetCoreDemoAPITests/ControllersTests/ValuesControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using NetCoreDemoAPI.Controllers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreDemoAPITests.ControllersTests
+{
+    [TestFixture]
+    public class ValuesControllerTest
+    {
+        protected ValuesController controller { get; private set; }
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            this.controller = new ValuesController();
+        }
+
+        [TestCase(2)]
+        public void AssertAreEqual(int id)
+        {
+            Assert.That(id, Is.Not.Null);
+            var value = controller.Get(id);
+            Assert.AreEqual(value, "value");
+        }
+    }
+}
